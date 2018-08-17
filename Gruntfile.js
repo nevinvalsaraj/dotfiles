8b70dc2bc85f962055c81fb2e664746faf142d5a
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2018-08-17 22:59:45 +0530

    Add task for zsh
    - Add grunt task for zsh
    - Refactor Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,31 +5,15 @@ var userhome = require('userhome');
 module.exports = function(grunt) {
 
     grunt.initConfig({
-
         config: {
             path_dotfiles: process.cwd(),
-
             neovim: {
                 path_vimplug: userhome('.local/share/nvim/site/autoload/plug.vim'),
                 url_vimplug: 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim',
-                path: '<%= config.path_dotfiles%>' + '/nvim',
-                path_system: userhome('.config/nvim')
             },
-
             vim: {
-                path_vimrc: '<%=  config.path_dotfiles%>' + '/vim/vimrc',
-                path_vimrc_system: userhome('.vimrc'),
-                path_vim: '<%= config.path_dotfiles%>' + '/vim',
-                path_vim_system: userhome('.vim'),
                 path_vundle: userhome('.vim/vundle/Vundle.vim'),
                 url_vundle: 'https://github.com/VundleVim/Vundle.vim.git'
-            },
-
-            git: {
-                path_gitconfig_system: userhome('.gitconfig'),
-                path_gitconfig: '<%= config.path_dotfiles%>' + '/git/gitconfig',
-                path_gitignore_system: userhome('.gitignore'),
-                path_gitignore: '<%= config.path_dotfiles%>' + '/git/gitignore'
             }
         },
 
@@ -50,31 +34,58 @@ module.exports = function(grunt) {
         },
 
         symlink: {
+            // neovim symlink
             neovim: {
-                dest: "<%= config.neovim.path_system%>",
-                relativeSrc: "<%= config.neovim.path%>",
+                dest: userhome('.config/nvim'),
+                relativeSrc: '<%= config.path_dotfiles%>' + '/nvim',
                 options: {type: 'dir'}
             },
 
+            // vim symlinks
             vimrc: {
-                dest: "<%= config.vim.path_vimrc_system%>",
-                relativeSrc: "<%= config.vim.path_vimrc%>"
+                dest: userhome('.vimrc'),
+                relativeSrc: '<%= config.path_dotfiles%>' + '/vim/vimrc'
             },
-
             vim: {
-                dest: "<%= config.vim.path_vim_system%>",
-                relativeSrc: "<%= config.vim.path_vim%>",
+                dest: userhome('.vim'),
+                relativeSrc: '<%= config.path_dotfiles%>' + '/vim',
                 options: {type: 'dir'}
             },
 
+            // git symlinks
             gitconfig: {
-                dest: "<%= config.git.path_gitconfig_system%>",
-                relativeSrc: "<%= config.git.path_gitconfig%>"
+                dest: userhome('.gitconfig'),
+                relativeSrc: '<%= config.path_dotfiles%>' + '/git/gitconfig'
+            },
+            gitignore: {
+                dest: userhome('.gitignore'),
+                relativeSrc: '<%= config.path_dotfiles%>' + '/git/gitignore'
             },
 
-            gitignore: {
-                dest: "<%= config.git.path_gitignore_system%>",
-                relativeSrc: "<%= config.git.path_gitignore%>"
+            // zsh symlinks
+            zlogin: {
+                dest: userhome('.zlogin'),
+                relativeSrc: '<%= config.path_dotfiles%>' + '/zsh/zlogin'
+            },
+            zshrc: {
+                dest: userhome('.zshrc'),
+                relativeSrc: '<%= config.path_dotfiles%>' + '/zsh/zshrc'
+            },
+            zshenv: {
+                dest: userhome('.zshenv'),
+                relativeSrc: '<%= config.path_dotfiles%>' + '/zsh/zshenv'
+            },
+            zprofile: {
+                dest: userhome('.zprofile'),
+                relativeSrc: '<%= config.path_dotfiles%>' + '/zsh/zprofile'
+            },
+            zpreztorc: {
+                dest: userhome('.zpreztorc'),
+                relativeSrc: '<%= config.path_dotfiles%>' + '/zsh/zpreztorc'
+            },
+            zlogout: {
+                dest: userhome('.zlogout'),
+                relativeSrc: '<%= config.path_dotfiles%>' + '/zsh/zlogout'
             }
         }
 
@@ -87,6 +98,14 @@ module.exports = function(grunt) {
     grunt.registerTask('neovim', ['curl:vimplug', 'symlink:neovim']);
     grunt.registerTask('vim', ['gitclone:vundle', 'symlink:vimrc', 'symlink:vim']);
     grunt.registerTask('git', ['symlink:gitconfig', 'symlink:gitignore']);
+    grunt.registerTask('zsh', [
+        'symlink:zlogin',
+        'symlink:zshrc',
+        'symlink:zshenv',
+        'symlink:zprofile',
+        'symlink:zpreztorc',
+        'symlink:zlogout'
+    ]);
     grunt.registerTask('banner', function(){
         grunt.log.writeln(grunt.file.read('templates/banner'));
     });
