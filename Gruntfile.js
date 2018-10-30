7dbbe909dd61c21bd0f81acf1b48d326a7a7d262
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2018-10-31 00:28:48 +0530

    Auto-create .config folder, add prezto repo clone
    - Clone prezto repo to the correct path on running zsh task.
    - Add step to create ~/.config folder during neovim task.
    - Remove unnecessary configuration values.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,35 +7,34 @@ module.exports = function(grunt) {
     grunt.initConfig({
         config: {
             path_dotfiles: process.cwd(),
-            neovim: {
-                path_vimplug: userhome('.local/share/nvim/site/autoload/plug.vim'),
-                url_vimplug: 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim',
-            },
-            vim: {
-                path_vundle: userhome('.vim/vundle/Vundle.vim'),
-                url_vundle: 'https://github.com/VundleVim/Vundle.vim.git'
-            }
         },
 
         curl: {
             vimplug: {
-                src: '<%= config.neovim.url_vimplug%>',
-                dest: '<%= config.neovim.path_vimplug%>'
+                src: 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim',
+                dest: userhome('.local/share/nvim/site/autoload/plug.vim')
             }
         },
 
         gitclone: {
             vundle: {
                 options: {
-                    directory: '<%= config.vim.path_vundle%>',
-                    repository: '<%= config.vim.url_vundle%>'
+                    directory: userhome('.vim/vundle/Vundle.vim'),
+                    repository: 'https://github.com/VundleVim/Vundle.vim.git'
+                }
+            },
+            prezto: {
+                options: {
+                    directory: userhome('.zprezto'),
+                    repository: 'https://github.com/sorin-ionescu/prezto.git',
+                    recursive: true
                 }
             }
         },
 
         symlink: {
             // neovim symlink
-            neovim: {
+            nvim: {
                 dest: userhome('.config/nvim'),
                 relativeSrc: '<%= config.path_dotfiles%>' + '/nvim',
                 options: {type: 'dir'}
@@ -87,18 +86,27 @@ module.exports = function(grunt) {
                 dest: userhome('.zlogout'),
                 relativeSrc: '<%= config.path_dotfiles%>' + '/zsh/zlogout'
             }
-        }
+        },
 
+        mkdir: {
+            dotconfig: {
+                options:{
+                    create: [userhome('.config')]
+                }
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-curl');
     grunt.loadNpmTasks('grunt-git');
     grunt.loadNpmTasks('grunt-symlink');
+    grunt.loadNpmTasks('grunt-mkdir');
 
-    grunt.registerTask('neovim', ['curl:vimplug', 'symlink:neovim']);
+    grunt.registerTask('neovim', ['curl:vimplug', 'mkdir:dotconfig', 'symlink:nvim']);
     grunt.registerTask('vim', ['gitclone:vundle', 'symlink:vimrc', 'symlink:vim']);
     grunt.registerTask('git', ['symlink:gitconfig', 'symlink:gitignore']);
     grunt.registerTask('zsh', [
+        'gitclone:prezto',
         'symlink:zlogin',
         'symlink:zshrc',
         'symlink:zshenv',
