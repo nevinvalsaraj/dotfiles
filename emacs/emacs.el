022ff70698cf86795325c8267e41600d7a4a58cc
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2021-02-11 21:16:41 +0530

    Add emacs configuration file
    Add basic emacs configuration file.
    
    First commit with magit!

---
+++ emacs/emacs.el
@@ -0,0 +1,92 @@
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(menu-bar-mode -1)
+
+
+;; Vertical Scroll
+(setq scroll-step 1)
+(setq scroll-margin 1)
+(setq scroll-conservatively 101)
+(setq scroll-up-aggressively 0.01)
+(setq scroll-down-aggressively 0.01)
+(setq auto-window-vscroll nil)
+(setq fast-but-imprecise-scrolling nil)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+(setq mouse-wheel-progressive-speed nil)
+;; Horizontal Scroll
+(setq hscroll-step 1)
+(setq hscroll-margin 1)
+
+
+(set-fringe-mode 20)
+(setq visible-bell t)
+(load-theme 'wombat)
+
+;; Add line numbers
+(column-number-mode)
+(global-display-line-numbers-mode 1)
+
+;; Indent on newline by default
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+;; Use package management features
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
+;; Use icons if running in GUI
+(use-package all-the-icons :if (display-graphic-p))
+
+;; Setup status bar
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+    :custom ((doom-modeline-height 15)))
+
+
+;; Load color theme
+(use-package doom-themes)
+(load-theme 'doom-vibrant t)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+
+;; Setup ivy, counsel and swiper
+(use-package ivy
+  :diminish
+  :init
+  (use-package amx :defer t)
+  (use-package counsel :diminish :config (counsel-mode 1))
+  (use-package swiper :defer t)
+  (ivy-mode 1)
+  :custom
+  (ivy-use-virtual-buffers t)
+  (ivy-wrap t))
+
+
+;; Enable hinting for keybinds
+(use-package which-key
+  :diminish
+  :custom
+  (which-key-separator " ")
+  (which-key-prefix-prefix "+")
+  :config
+  (which-key-mode))
+
+(use-package magit)
