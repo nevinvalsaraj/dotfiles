3e0e1d9432931d0e2b83a51d8ef0c57b4c94393c
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2021-03-05 19:51:25 +0530

    Add auto-gen lines to emacs.el, add npm start
    Add auto-generated lines to emacs configuration file and add `npm
    start` command to simplify dotfiles setup.

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -90,3 +90,17 @@
   (which-key-mode))
 
 (use-package magit)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit which-key counsel amx ivy rainbow-delimiters doom-themes doom-modeline all-the-icons use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
